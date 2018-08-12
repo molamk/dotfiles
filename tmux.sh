c15123c0f269f98533f853485d61cfdeadab9583
molamk <molamk@protonmail.com>
2018-08-12 23:45:41 +0000

    clean up

--- tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Tmux
-mkdir -p ~/.tmux
-git clone https://github.com/seebi/tmux-colors-solarized ~/.tmux/tmux-colors-solarized
-cat ~/.tmux/tmux-colors-solarized/tmuxcolors-256.conf >> ~/.tmux.conf
-
